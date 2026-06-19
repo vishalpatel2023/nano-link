0c41126e6c646482bde8da2aee037211c98f959e
Vishal Patel <vishalpatelvns3314@gmail.com>
2026-06-19 10:59:54 +0530

    improved ui and other logics added

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const path = require('path');
 
+const jwt = require('jsonwebtoken');
+const User = require('./models/user');
 
 const urlRoutes = require('./routes/urlRoutes');
 const urlController = require('./controllers/urlController');
 const userRoutes = require('./routes/userRoutes');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
@@ -21,6 +24,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(express.json());
+app.use(cookieParser());
+
+// global middleware to pass user data to all EJS views
+app.use(async (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                next();
+            } else {
+                // email ko DB se nikaalo
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user; 
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+});
+
 app.use('/', userRoutes);
 
 
--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {createShortUrl, redirectUrl} = require('../controllers/urlController');
+const { requireAuth } = require('../middlewares/auth'); // <-- MIDDLEWARE
 
-router.post('/shorten', createShortUrl);
+router.post('/shorten', requireAuth, createShortUrl);
 
 router.get('/:shortCode', redirectUrl);
 
